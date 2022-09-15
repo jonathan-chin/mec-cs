e67bb678f74e332e00e468c72c2ef67551de4748
Jon Chin <jon@sharebyteaching.org>
2022-09-14 22:12:16 -0400

    added

---
+++ 2022-fall/cs-350-mowe/slides/01-introduction-code-sample.js
@@ -0,0 +1,51 @@
+// let's create some classes to allow modeling how companies, banks, and employees all interact
+
+class Employee{
+    // fields
+    // for classes, don't need to declare variables with var / let / const
+
+    // can prepend variable with # to mark as private
+    #ssn;
+    name;
+    position;
+
+    // default value for account_balance field
+    account_balance = 0;
+
+    // JS objects have constructors
+    constructor(name){
+	this.name = name;
+    }
+}
+
+class Company{
+    name;
+    employees = [];
+    funds = 1000000;
+    constructor(name){
+	this.name = name;
+    }
+}
+
+// can use `new` to instantiate a class
+const Jon = new Employee('Jon');
+const ShareMeals = new Company('Share Meals');
+console.log(ShareMeals.employees);
+ShareMeals.employees.push(Jon);
+console.log(ShareMeals.employees);
+
+pay(Jon);
+console.log(Jon.account_balance);
+
+console.log(ShareMeals.employees);
+
+let {account_balance} = Jon;
+account_balance += 100;
+console.log(account_balance);
+console.log(Jon.account_balance);
+
+
+
+function pay(who){
+    who.account_balance += 100;
+}
