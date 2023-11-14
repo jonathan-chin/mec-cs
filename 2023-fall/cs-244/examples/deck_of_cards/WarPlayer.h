abe5fd0aaa81b89cb1bc5b7b2dbe1fdcaca0aa31
Jon Chin <jon@sharebyteaching.org>
2023-11-14 14:09:02 -0500

    fixed typo

--- 2023-fall/cs-244/examples/deck_of_cards/WarPlayer.h
+++ 2023-fall/cs-244/examples/deck_of_cards/WarPlayer.h
@@ -1,7 +1,7 @@
 #ifndef WARPLAYER_H
 #define WARPLAYER_H
 #include "Player.h"
-#include "Card.h";
+#include "Card.h"
 
 
 class WarPlayer : public Player{ // inheritance is public by default; may need to change
