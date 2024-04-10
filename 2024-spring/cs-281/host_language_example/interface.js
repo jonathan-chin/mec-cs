62831771d1e463e0af457205cf114bd5e0b500e9
Jon Chin <jon@sharebyteaching.org>
2024-04-10 10:20:29 -0400

    added

---
+++ 2024-spring/cs-281/host_language_example/interface.js
@@ -0,0 +1,38 @@
+const {Client} = require('pg');
+require('dotenv').config();
+const fs = require('fs');
+const prompt = require('prompt-sync')();
+
+
+const client = new Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    ssl: true
+});
+
+client.connect((error) => {
+    if(error){
+        console.log(error);
+    }
+});
+
+const run = async () => {
+    
+    let input;
+    let sql;
+
+    do{
+	input = prompt('type in the name of the sql file you want to run or exit to stop ');
+	if(input === 'exit'){
+	    break;
+	}
+	sql = fs.readFileSync(`./${input}.sql`).toString();
+	const result = await client.query(sql);
+        console.log(JSON.stringify(result, null, 4));
+    }while(input !== 'exit');
+};
+
+run();
